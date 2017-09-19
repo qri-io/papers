f8de7eca861c07987eab7838643cda3eb728bcf0
Michael Hucka <mhucka@caltech.edu>
2017-09-19 09:22:17 -0700

    Reverting to insecure but working version

--- qri-deterministic_querying/index.php
+++ qri-deterministic_querying/index.php
@@ -1,9 +1,10 @@
 <?php
 $username = "research";
-$passhash = "1fcb5d7e140ff89ce257390b881f5c32";
+$password = "the coolest";
+$nonsense = "supercalifragilisticexpialidocious";
 
 if (isset($_COOKIE['PrivatePageLogin'])) {
-    if ($_COOKIE['PrivatePageLogin'] == $passhash)) {
+    if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
         include_once("./v2.html");
         exit;
     } else {
@@ -16,21 +17,19 @@ if (isset($_GET['p']) && $_GET['p'] == "login") {
     if ($_POST['user'] != $username) {
         echo "Sorry, that username does not match.";
         exit;
-    } else if (md5($_POST['keypass']) != $passhash) {
-        echo "Sorry, that passhash does not match.";
+    } else if ($_POST['keypass'] != $password) {
+        echo "Sorry, that password does not match.";
         exit;
-    } else if ($_POST['user'] == $username && md5($_POST['keypass']) == $passhash) {
-        setcookie('PrivatePageLogin', md5($_POST['keypass']));
+    } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
+        setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
         header("Location: $_SERVER[PHP_SELF]");
     } else {
         echo "Sorry, you could not be logged in at this time.";
     }
 }
 ?>
-<center>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
 <label><input type="text" name="user" id="user" /> Name</label><br />
-<label><input type="passhash" name="keypass" id="keypass" /> Passhash</label><br />
+<label><input type="password" name="keypass" id="keypass" /> Password</label><br />
 <input type="submit" id="submit" value="Login" />
 </form>
-</center>
